524e421b8c1a3d262dc0de9042de2b0d05bcfae6
halac123b <halac123b@gmail.com>
2024-09-25 00:11:25 +0700

    Add m_1

---
+++ 1/main.py
@@ -0,0 +1,8 @@
+import torch
+import torchvision  # Lib của Pytorch handle các data thuộc vision domain
+
+# datasets: access database of Pytorch
+## FashionMNIST: dataset về fashion
+training_data = torchvision.datasets.FashionMNIST(
+
+)
